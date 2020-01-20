ef258ecb1359dd72a32cd1dd8ba3c8b0adc953ba
Alex Rudd <rudd.alex1@gmail.com>
2020-01-20 15:45:02 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alexrudd/golangci-lint-teamcity
+
+go 1.13
+
+require github.com/golangci/golangci-lint v1.23.1
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/golangci/golangci-lint/pkg/printers"
+)
+
+func main() {
+	results, err := readResults(bufio.NewReader(os.Stdin))
+	if err != nil {
+		fmt.Printf("reading results: %s", err)
+		os.Exit(1)
+	}
+
+	writeServiceMessages(os.Stdout, results)
+}
+
+func readResults(r io.Reader) (*printers.JSONResult, error) {
+	result := &printers.JSONResult{}
+
+	err := json.NewDecoder(r).Decode(result)
+	if err != nil {
+		return nil, fmt.Errorf("decoding input: %w", err)
+	}
+
+	return result, nil
+}
+
+const (
+	timestampFormat = "2006-01-02T15:04:05.000"
+	testStarted     = "##teamcity[testStarted timestamp='%s' name='%s']"
+	testStdErr      = "##teamcity[testStdErr timestamp='%s' name='%s' out='%s']"
+	testFailed      = "##teamcity[testFailed timestamp='%s' name='%s']"
+	testIgnored     = "##teamcity[testIgnored timestamp='%s' name='%s']"
+	testFinished    = "##teamcity[testFinished timestamp='%s' name='%s']"
+)
+
+func getNow() string {
+	return time.Now().Format(timestampFormat)
+}
+
+func mustFprintln(w io.Writer, a ...interface{}) {
+	_, err := fmt.Fprintln(w, a...)
+	if err != nil {
+		panic("writing to io.Writer: " + err.Error())
+	}
+}
+
+func writeServiceMessages(w io.Writer, results *printers.JSONResult) {
+	failedTests := map[string]bool{}
+
+	for _, linter := range results.Report.Linters {
+		if linter.Enabled {
+			mustFprintln(w, fmt.Sprintf(testStarted, getNow(), linter.Name))
+
+			failedTests[linter.Name] = false
+		} else {
+			mustFprintln(w, fmt.Sprintf(testIgnored, getNow(), linter.Name))
+		}
+	}
+
+	for _, issue := range results.Issues {
+		mustFprintln(w, fmt.Sprintf(testStdErr, getNow(), issue.FromLinter,
+			fmt.Sprintf("%s:%v - %s", issue.FilePath(), issue.Line(), issue.Text),
+		))
+
+		failedTests[issue.FromLinter] = true
+	}
+
+	for _, linter := range results.Report.Linters {
+		if failedTests[linter.Name] {
+			mustFprintln(w, fmt.Sprintf(testFailed, getNow(), linter.Name))
+		} else {
+			mustFprintln(w, fmt.Sprintf(testFinished, getNow(), linter.Name))
+		}
+	}
+}
